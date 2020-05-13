bfdd0e7181ab12293df3950b973f19fea1c89d35
FernandaHamada <fernandakubayashi@gmail.com>
2020-05-13 11:45:23 -0300

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({  //decorator
+  selector: 'app-root', // permite utilizar o componente e template, em sua forma declarativa
+  templateUrl: './app.component.html', // apresentação desse componente
+  styleUrls: ['./app.component.css'] // local onde fica o CSS
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { // só é um componente pq está anotado com um @Component
+
+  title = 'alurapic'; //Data binding
+  description = "Leão";
+  url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg"
 }
