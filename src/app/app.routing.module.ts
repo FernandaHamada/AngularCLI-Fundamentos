543dc69e9eb61b08dcac5ae6058f554ba70dd3f3
FernandaHamada <fernandakubayashi@gmail.com>
2020-05-16 23:27:18 -0300

    Registro de Novos Usuários

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,12 +7,14 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
 import { AuthGuard } from './core/auth/auth.guard';
+import { SignUpComponent } from './home/signup/signup.component';
 
 
 //configuração de rotas
 // o framework Angular interpreta essa URL e verifica se há um roteamento associado
 const routes: Routes =[
     { path: '', component: SignInComponent, canActivate: [AuthGuard]},
+    { path: 'signup', component: SignUpComponent},
     { path: 'user/:userName', component: PhotoListComponent, resolve:{ photos: PhotoListResolver}},
     { path: 'p/add', component: PhotoFormComponent},
     { path: '**', component: NotFoundComponent}
