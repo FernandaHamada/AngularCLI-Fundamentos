c4308b725ea4f71b4efe58b4b65e956caaeb29f6
FernandaHamada <fernandakubayashi@gmail.com>
2020-05-31 20:02:51 -0300

    Upload e progresso

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
+
+import { PhotoService } from '../photo/photo.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { UserService } from 'src/app/core/user/user.service';
 
@@ -15,9 +18,10 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone = 0;
   constructor(
-    private formBuilder: FormBuilder, 
-    private photoService: PhotoService, 
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
     private router: Router,
     private alertService: AlertService,
     private userService: UserService) { }
@@ -34,16 +38,29 @@ export class PhotoFormComponent implements OnInit {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService.upload(description, allowComments, this.file)
-      .subscribe(() => {
-        this.alertService.success('Upload completed', true);
+      .pipe(finalize(() => {
         this.router.navigate(['/user', this.userService.getUserName()])
+      }))
+      .subscribe((event: HttpEvent<any>) => {
+        if (event.type == HttpEventType.UploadProgress) {
+          this.percentDone = Math.round(100 * event.loaded / event.total);
+        } else if (event instanceof HttpResponse) {
+          this.alertService.success('Upload completed', true);
+        }
+      },
+      err => {
+        console.log(err);
+        this.alertService.danger('Upload error!', true);
       });
   }
 
-  handleFile(file: File){
+  // O operador Finalize do RXJS, garante que um trecho de código seja executado tanto após o sucesso 
+  // ou fracasso da operação
+
+  handleFile(file: File) {
     this.file = file;
     const reader = new FileReader();
-    reader.onload = (event: any ) => this.preview = event.target.result;
+    reader.onload = (event: any) => this.preview = event.target.result;
     reader.readAsDataURL(file);
   }
 
